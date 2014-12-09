30ac6aacd3bcb17fa786aaf913496043a6568be4
philopian <pdotwdot@gmail.com>
2014-12-08 16:45:05 -0800

    Initial Commit

---
+++ TakeOrSelectPhotoTests/TakeOrSelectPhotoTests.m
@@ -0,0 +1,40 @@
+//
+//  TakeOrSelectPhotoTests.m
+//  TakeOrSelectPhotoTests
+//
+//  Created by Philippe Willis on 12/8/14.
+//  Copyright (c) 2014 Philippe Willis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TakeOrSelectPhotoTests : XCTestCase
+
+@end
+
+@implementation TakeOrSelectPhotoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
