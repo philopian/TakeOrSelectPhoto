b0edb62a9b50aaa14f72b251f547ded7194d108f
philopian <pdotwdot@gmail.com>
2014-12-08 17:08:25 -0800

    "take photo" button and styled it

--- TakeOrSelectPhoto/ViewController.m
+++ TakeOrSelectPhoto/ViewController.m
@@ -1,14 +1,14 @@
-//
-//  ViewController.m
-//  TakeOrSelectPhoto
-//
-//  Created by Philippe Willis on 12/8/14.
-//  Copyright (c) 2014 Philippe Willis. All rights reserved.
-//
+#define RGB(r, g, b)     [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
+#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+
 
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)takePhoto:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *takePhoto;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewSelect;
 
 @end
 
@@ -17,11 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.takePhoto.layer.cornerRadius = 6;
+    self.takePhoto.clipsToBounds = YES;
+    self.takePhoto.backgroundColor = RGB(39, 41, 42);
+    self.takePhoto.layer.borderColor = self.takePhoto.tintColor.CGColor;
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)takePhoto:(id)sender
+{
+    
+}
+
+
+
 
 @end
